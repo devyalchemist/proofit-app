5b9e8bbe2d31d5100472701490ae5984d2857c7e
devyalchemist <devyalchemist@gmail.com>
2025-07-01 09:19:03 +0100

    provide navigation for the home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,14 @@
+import { Link } from "react-router";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex justify-between px-18 py-4">
+      Home
+      <Link to={"/login"}>
+        <button className="rounded-[12px] bg-blue-600 px-6 py-2 text-white">
+          login
+        </button>
+      </Link>
+    </div>
+  );
 }
