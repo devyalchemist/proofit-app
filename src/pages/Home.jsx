3f4262927251b7a1c9c7ad9e52710cc58971d9c7
devyalchemist <devyalchemist@gmail.com>
2025-07-10 08:24:42 +0100

    Update Home.jsx
    changed the background path from images to /images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import HomeHeader from "../ui/HomeHeader";
 
 export default function Home() {
   return (
-    <div className="bg-background/99 relative grid h-[100dvh] grid-rows-[auto_1fr_auto] bg-[url('images/workout-1.jpg')] bg-blend-overlay  inset-0 bg-cover bg-center bg-">
+    <div className="bg-background/99 relative grid h-[100dvh] grid-rows-[auto_1fr_auto] bg-[url('/images/workout-1.jpg')] bg-blend-overlay  inset-0 bg-cover bg-center bg-">
       <HomeHeader />
       <HomeBody />
       <Footer />
